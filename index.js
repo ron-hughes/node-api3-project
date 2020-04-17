71df063c6322acf1a60754716dbd6c9c2c568db6
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2020-04-17 12:31:45 -0400

    updated

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code away!
+const express = require('express');
+const userRouter = require('./users/userRouter.js')
+const postRouter = require('./posts/postRouter.js')
+const server = express();
+const port = 4000
+
+
+server.use("/api/users", userRouter)
+server.use("/api/posts", postRouter)
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,54 @@
-const express = require('express');
-
+const express = require("express");
+const db = require("./userDb.js");
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", async  (req, res, next) => {
+try {
+  const data = await db.insert (req.body )
+  res.status(201).json(data)
+} catch (err) {
+  next(err)
+}
+// console.log(req.body)
+
+});
+
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", async (req, res, next) => {
+  try { 
+    const users = await db.get()
+    res.status(200).json(users);
+  }
+  catch (err) {
+    next()
+  }
+  
+
+});
+
+router.get("/:id", (req, res, next) => {
+  try {
+    const data = db.getById(req.params.id)
+    res.status(200).json(data)
+  }
+  catch (err) {
+    next(err)
+  }
+
+});
+
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
